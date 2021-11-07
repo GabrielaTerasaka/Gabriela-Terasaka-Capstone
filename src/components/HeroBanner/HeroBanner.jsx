84e63db6d6fd679f2c16a30f110175e5005c574d
Gabriela Terasaka <gabriela.terasaka@gmail.com>
2021-11-07 17:07:39 -0500

    add hero banner component:

---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import "./HeroBanner.scss";
+import React from "react";
+
+function HeroBanner() {
+  return (
+    <section className="hero">
+      <h2 className="hero__title">Let's Organize Our Kitchen</h2>
+    </section>
+  );
+}
+
+export default HeroBanner;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 import cookingImage from "../../assets/images/becca-tapert-O7sK3d3TPWQ-unsplash.jpg";
 import "./HomePage.scss";
 
-import HomeHeader from "../../components/HomeHeader/HomeHeader";
+import HomeHeader from "../../components/HomeHeader";
+import HeroBanner from "../../components/HeroBanner";
+
 export default function HomePage() {
   return (
     <div>
       {" "}
       <HomeHeader />
+      <HeroBanner />
       <h1>Welcome</h1>
       <img src={cookingImage} alt="" className="image" />
     </div>
