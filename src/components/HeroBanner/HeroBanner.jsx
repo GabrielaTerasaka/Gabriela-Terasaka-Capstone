0b8d7c3ae263429f0075bed2c76d93902f976945
Gabriela Terasaka <gabriela.terasaka@gmail.com>
2021-11-07 22:56:28 -0500

    UPDATE HOMEHEADER AND HERO COMPONENTS

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function HeroBanner() {
   return (
     <section className="hero">
-      <h2 className="hero__title">Let's Organize Our Kitchen</h2>
+      <h2 className="hero__title">Let's Organize Your Kitchen</h2>
     </section>
   );
 }
